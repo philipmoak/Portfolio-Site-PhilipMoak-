e86a8f0641f8d657f90b807023499614e6982fc8
philipmoak <106561040+philipmoak@users.noreply.github.com>
2022-11-10 16:23:20 -0800

    edited code
    edited code one more time

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,9 @@ console.log("Hey! This is my portfolio")
 $(document).ready(function(){
   $(".navigation").click(function(){
   console.log("Clicked")
+  console.log($(window).width())
+  if($(window).width() < 642) {
     $(".mobile-menu").toggle();
-    $(".navigation").toggleClass("navigate-padding")
+  }
   })
 });
